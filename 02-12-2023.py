7ce2fe3c9d85c850262d90fc7f659e65a808d2e5
antoine <antoinecaudron8@gmail.com>
2023-12-05 12:00:44 +0100

    Finish Part 2 of 02-12-2023.py

--- 02-12-2023.py
+++ 02-12-2023.py
@@ -32,5 +32,35 @@ def part1() -> int:
 
     return ans
 
+def part2() -> int :
 
-print(part1())
+    ans = 0
+
+    f = open("adventofcode.com_2023_day_2_input.txt", "r")
+
+    for l in f:
+
+        gameId = int(re.search(r'\d+', l.split(":")[0]).group())
+
+        splitL = l.split(":")
+        splitL.pop(0)
+
+        maxRed, maxBlue, maxGreen = 0, 0, 0
+        for set in splitL[0].split(";"):
+
+            for cubes in set.split(","):
+
+                intCube = int(cubes.split()[0])
+
+                if cubes.split()[1] == "red" and intCube > maxRed:
+                    maxRed = intCube
+                if cubes.split()[1] == "blue" and intCube > maxBlue:
+                    maxBlue = intCube
+                if cubes.split()[1] == "green" and intCube > maxGreen:
+                    maxGreen = intCube
+
+        ans += maxRed * maxGreen * maxBlue
+
+    return ans
+
+print(part2())
