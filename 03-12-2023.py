0b7753adfd57636cd928238303ed01c28275862c
antoine <antoinecaudron8@gmail.com>
2023-12-06 16:36:24 +0100

    Advance Day 3

--- 03-12-2023.py
+++ 03-12-2023.py
@@ -1,11 +1,28 @@
-import re
+def getNeighboors(array, row_number, colmun_numver):
+    print()
+
 
 f = open("adventofcode.com_2023_day_3_input.txt", "r")
 
-data = []
 ans = 0
-for row in f:
-    data.append([x for x in row.split()])
+specialChar = ['*', '#', '+', '$']
+data = []
 
+for row in f:
+    for x in row.split():
+        data.append(list(x))
+
+print("Affichage de la matrice : \n")
 for row in data:
     print(row)
+print("\n")
+
+for i in range(0, len(data), 1):
+    print(data[i])
+    for j in range(0, len(data[i]), 1):
+        if data[i][j] in specialChar:
+            getNeighboors(data, i, j)
+
+    if i == 2:
+        exit()
+
